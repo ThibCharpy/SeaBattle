3a6ba968f61591db9dc5f223c6267d286559f0aa
ThibCharpy <thibault.charpignon@wanadoo.fr>
2016-12-22 16:38:40 +0100

    merge with loginlayout Branch

--- app/src/main/java/fr/univ/orleans/android/seabattle/views/MainActivity.java
+++ app/src/main/java/fr/univ/orleans/android/seabattle/views/MainActivity.java
@@ -1,7 +1,6 @@
 package fr.univ.orleans.android.seabattle.views;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
