db6680d6dfc4514742b1c84472f49f5c066ad9df
ThibCharpy <thibault.charpignon@wanadoo.fr>
2016-12-22 15:44:58 +0100

    long click and short click implemented

--- app/src/main/java/fr/univ/orleans/android/seabattle/views/MainActivity.java
+++ app/src/main/java/fr/univ/orleans/android/seabattle/views/MainActivity.java
@@ -1,5 +1,6 @@
 package fr.univ.orleans.android.seabattle.views;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -19,11 +20,13 @@ import fr.univ.orleans.android.seabattle.controller.Controller;
 import fr.univ.orleans.android.seabattle.database.ProfilsDataSource;
 import fr.univ.orleans.android.seabattle.model.Profil;
 
-public class MainActivity extends ListActivity implements AdapterView.OnItemLongClickListener {
+public class MainActivity extends Activity {
 
     private Controller controller;
 
     private ProfilsDataSource dataSource;
+
+    ListView listView;
 
     EditText username;
 
@@ -35,6 +38,23 @@ public class MainActivity extends ListActivity implements AdapterView.OnItemLong
         this.controller = new Controller();
         this.username = (EditText) findViewById(R.id.username);
 
+        this.listView = (ListView) findViewById(R.id.listProfils);
+        this.listView.setLongClickable(true);
+        this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "short clicked pos: " + position, Toast.LENGTH_LONG).show();
+            }
+        });
+        this.listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "long clicked pos: " + position, Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+
         this.dataSource = new ProfilsDataSource(this);
         this.dataSource.open();
 
@@ -43,12 +63,12 @@ public class MainActivity extends ListActivity implements AdapterView.OnItemLong
         ArrayAdapter<Profil> adapter = new ArrayAdapter<Profil>(this,
                 android.R.layout.simple_list_item_1, profils);
 
-        setListAdapter(adapter);
+        this.listView.setAdapter(adapter);
 
     }
 
     public void add(View view) {
-        ArrayAdapter<Profil> adapter = (ArrayAdapter<Profil>) getListAdapter();
+        ArrayAdapter<Profil> adapter = (ArrayAdapter<Profil>) this.listView.getAdapter();
         Profil profil = null;
 
         Editable username = this.username.getText();
@@ -63,12 +83,5 @@ public class MainActivity extends ListActivity implements AdapterView.OnItemLong
             LinearLayout input_zone = (LinearLayout) findViewById(R.id.input_username);
             input_zone.setVisibility(Button.GONE);
         }
-    }
-
-
-    @Override
-    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(this, "long clicked pos: " + position, Toast.LENGTH_LONG).show();
-        return true;
     }
 }
